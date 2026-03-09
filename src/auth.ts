41fa09cb0803c843eb13685fe2e6dae00600faa9
Lana <jade12052024@gmail.com>
2026-03-09 20:52:35 +0700

    feat: Add initial authentication configuration in `src/auth.ts` and include related development server logs.

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,10 @@
-import SpotifyProvider from "next-auth/providers/spotify";
+import Spotify from "next-auth/providers/spotify";
 import NextAuth from "next-auth";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     trustHost: true,
     providers: [
-        SpotifyProvider({
-            clientId: process.env.SPOTIFY_CLIENT_ID || "",
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
+        Spotify({
             authorization: {
                 params: {
                     scope: "user-read-currently-playing user-read-playback-state user-modify-playback-state streaming user-read-email user-read-private",
